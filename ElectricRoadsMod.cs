f95b665627baae0ffad67f71a776d87f79a3f0a3
Leandro Klyte <klyte45@hotmail.com>
2020-04-07 15:43:28 -0300

    v2.0.0 r15  - Fixed "What's New" overlapping fail

--- ElectricRoadsMod.cs
+++ ElectricRoadsMod.cs
@@ -7,7 +7,7 @@ using Klyte.Commons.Utils;
 using Klyte.ElectricRoads.Overrides;
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.0.0.14")]
+[assembly: AssemblyVersion("2.0.0.15")]
 namespace Klyte.ElectricRoads
 {
     public class ElectricRoadsMod : BasicIUserMod<ElectricRoadsMod, ElectricRoadsController, ERPanel>
