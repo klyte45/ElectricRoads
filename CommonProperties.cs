dc8c42f007f51f89cde5d517ba3a12fc2bbd53e9
Leandro Klyte <klyte45@hotmail.com>
2022-01-16 15:39:52 -0300

    Updating to latest commons

--- CommonProperties.cs
+++ CommonProperties.cs
@@ -1,19 +1,25 @@
+using Klyte.Commons.Utils;
 using Klyte.ElectricRoads;
+using UnityEngine;
 
 namespace Klyte.Commons
 {
     public static class CommonProperties
     {
-        public static bool DebugMode { get; } = ElectricRoadsMod.DebugMode;
-        public static string Version { get; } = ElectricRoadsMod.Version;
-        public static string ModName { get; } = ElectricRoadsMod.Instance.SimpleName;
-        public static string Acronym { get; } = "ER";
-        public static string ModRootFolder { get; } = ElectricRoadsController.FOLDER_PATH;
+        public static bool DebugMode => ElectricRoadsMod.DebugMode;
+        public static string Version => ElectricRoadsMod.Version;
+        public static string ModName => ElectricRoadsMod.Instance.SimpleName;
+        public static string Acronym => "ER";
+        public static string ModRootFolder => ElectricRoadsController.FOLDER_PATH;
+        public static string ModDllRootFolder => ElectricRoadsMod.RootFolder;
 
-        public static string GitHubRepoPath { get; } = "klyte45/ElectricRoads";
-        public static string[] AssetExtraDirectoryNames { get; } = new string[0];
-        public static string[] AssetExtraFileNames { get; } = new string[0];
-        public static string ModIcon { get; } = ElectricRoadsMod.Instance.IconName;
-        public static string ModDllRootFolder { get; } = ElectricRoadsMod.RootFolder;
+        public static string[] AssetExtraDirectoryNames => new string[0];
+        public static string[] AssetExtraFileNames => new string[0];
+        public static string ModIcon => ElectricRoadsMod.Instance.IconName;
+        public static string GitHubRepoPath => "klyte45/ElectricRoads";
+
+        public static float UIScale { get; } = 1f;
+        public static Color ModColor { get; } = ColorExtensions.FromRGB("643E00");
+        public static MonoBehaviour Controller => ElectricRoadsMod.Controller;
     }
 }
